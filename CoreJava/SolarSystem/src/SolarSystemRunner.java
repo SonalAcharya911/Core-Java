f2cce929310856856aed39fda88b55db94359c7b
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-03 15:05:43 +0530

    solarSystem task

--- CoreJava/SolarSystem/src/SolarSystemRunner.java
+++ CoreJava/SolarSystem/src/SolarSystemRunner.java
@@ -1,12 +1,12 @@
 public class SolarSystemRunner {
     public static void main(String[] args) {
-        SolarSystem solarSystem1=new SolarSystem("Milkyway");
+        SolarSystem solarSystem1=new SolarSystem("Milky Way");
         solarSystem1.info();
 
         SolarSystem solarSystem2=new SolarSystem(8,290);
         solarSystem2.info();
 
-        SolarSystem solarSystem3=new SolarSystem("Milkyway","Jupiter","Mercury");
+        SolarSystem solarSystem3=new SolarSystem("Milky Way","Jupiter","Mercury");
         solarSystem3.info();
 
         SolarSystem solarSystem4=new SolarSystem("Earth",143000000,12470);
