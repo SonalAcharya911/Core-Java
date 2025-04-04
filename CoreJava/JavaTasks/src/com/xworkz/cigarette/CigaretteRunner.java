b0f5548571879d697ed2cc20810f26dedf29b991
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-04 10:50:15 +0530

    pushing tasks(03-04-2025) to git

--- CoreJava/JavaTasks/src/com/xworkz/cigarette/CigaretteRunner.java
+++ CoreJava/JavaTasks/src/com/xworkz/cigarette/CigaretteRunner.java
@@ -4,9 +4,8 @@ import java.util.Objects;
 
 public class CigaretteRunner {
     public static void main(String[] args) {
-        Cigarette cigarette=new Cigarette();
         Smoker smoker=new Smoker();
-
+        Cigarette cigarette=new Cigarette();
         if(Objects.nonNull(smoker)){
             smoker.smokes(cigarette);
         }
--- CoreJava/JavaTasks/src/com/xworkz/external/Gold.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.external;
-
-public class Gold {
-    public void carat() {
-        System.out.println("carat running in Gold");
-    }
-}
