1e9e2251867363c56d5d64b6451a450a02c3f2b8
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-06 23:55:42 +0530

    pushing tasks(04-04-2025) to git

--- CoreJava/JavaTasks/src/com/xworkz/property/PropertyRunner.java
+++ CoreJava/JavaTasks/src/com/xworkz/property/PropertyRunner.java
@@ -5,10 +5,13 @@ public class PropertyRunner {
         Property property1=new Property(120,"Kaggadasapura");
         Property property2=new Property(240,"Brookefield");
 
-        Property[] property={property1,property1};
+        Property[] property={property1,property2};
 
         Owner owner=new Owner("Sonal",21,property);
         owner.show();
+
+        Owner owner1=new Owner("Saahil",18,property);
+        owner1.show();
     }
 
 }
