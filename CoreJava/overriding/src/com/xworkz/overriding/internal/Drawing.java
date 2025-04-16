e59a0fbd2561d5243628b8794b1d548b4c1d4ad2
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-16 23:58:59 +0530

    overriding toString method

--- CoreJava/overriding/src/com/xworkz/overriding/internal/Drawing.java
+++ CoreJava/overriding/src/com/xworkz/overriding/internal/Drawing.java
@@ -1,6 +1,21 @@
 package com.xworkz.overriding.internal;
 
 public class Drawing {
+    private String name;
+    private boolean pencilSketch;
+
+    public Drawing(String name,boolean pencilSketch){
+        this.name=name;
+        this.pencilSketch=pencilSketch;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("name: " + name);
+        System.out.println("pencilSketch: " + pencilSketch);
+        return "";
+    }
+
     public Drawing(){
         System.out.println("no-arg constructor for Drawing");
     }
