5a3f36ca499745569159e6a67ec3f7b3251019ac
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-16 23:46:19 +0530

    overriding toString method

--- CoreJava/overriding/src/com/xworkz/overriding/internal/Container.java
+++ CoreJava/overriding/src/com/xworkz/overriding/internal/Container.java
@@ -4,7 +4,17 @@ public class Container {
     private double capacity;
 
     private String material;
+    public Container(double capacity,String material){
+        this.capacity=capacity;
+        this.material=material;
+    }
 
+    @Override
+    public String toString() {
+        System.out.println("capacity: " + capacity);
+        System.out.println("material:  " + material);
+        return "";
+    }
 
     public Container(){
         System.out.println("no-arg constructor for Container");
--- CoreJava/overriding/src/com/xworkz/overriding/internal/Costume.java
+++ CoreJava/overriding/src/com/xworkz/overriding/internal/Costume.java
@@ -1,6 +1,22 @@
 package com.xworkz.overriding.internal;
 
 public class Costume {
+    private String design;
+
+    private String fabricType;
+
+    public Costume(String design,String fabricType){
+        this.design=design;
+        this.fabricType=fabricType;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("design: " + design);
+        System.out.println("fabricType: " + fabricType);
+        return "";
+    }
+
     public Costume(){
         System.out.println("no-arg constructor for Costume");
     }
