63465a6e6ab30aec48ed90deab9e0074d90139b5
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-17 00:00:56 +0530

    overriding toString method

--- CoreJava/overriding/src/com/xworkz/overriding/internal/Fabric.java
+++ CoreJava/overriding/src/com/xworkz/overriding/internal/Fabric.java
@@ -1,6 +1,22 @@
 package com.xworkz.overriding.internal;
 
 public class Fabric {
+    private double length;
+
+    private String material;
+
+    public Fabric(double length,String material){
+        this.length=length;
+        this.material=material;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("length: " + length);
+        System.out.println("material: " + material);
+        return "";
+    }
+
     public Fabric(){
         System.out.println("no-arg constructor for Fabric");
     }
