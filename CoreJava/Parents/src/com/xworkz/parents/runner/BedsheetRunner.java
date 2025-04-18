8dba7c99744ee7e3f8dea652dc32f2d3af779e2b
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-18 09:00:44 +0530

    overriding equals from Object

--- CoreJava/Parents/src/com/xworkz/parents/runner/BedsheetRunner.java
+++ CoreJava/Parents/src/com/xworkz/parents/runner/BedsheetRunner.java
@@ -4,16 +4,14 @@ import com.xworkz.parents.internal.Bedsheet;
 
 public class BedsheetRunner {
     public static void main(String[] args) {
-        Bedsheet bedsheet=new Bedsheet("Cotton","Blue",750,"Double");
-        Bedsheet bedsheet1=new Bedsheet("Microfibre","yellow",450,"single");
-        Bedsheet bedsheet2=new Bedsheet("Cotton","Blue",750,"Double");
+        Bedsheet bedsheet = new Bedsheet("Cotton", "Blue", 750, "Double");
+        Bedsheet bedsheet1 = new Bedsheet("Microfibre", "yellow", 450, "single");
+        Bedsheet bedsheet2 = new Bedsheet("Cotton", "Blue", 750, "Double");
 
-        boolean match=bedsheet.equals(bedsheet2);
-        boolean notMatch=bedsheet.equals(bedsheet1);
+        boolean match = bedsheet.equals(bedsheet2);
+        boolean notMatch = bedsheet.equals(bedsheet1);
 
-        System.out.println("match: "+match);
-        System.out.println("notMatch: "+notMatch);
+        System.out.println("match: " + match);
+        System.out.println("notMatch: " + notMatch);
     }
-
-
 }
--- CoreJava/Parents/src/com/xworkz/parents/runner/ToothPasteRunner.java
+++ CoreJava/Parents/src/com/xworkz/parents/runner/ToothPasteRunner.java
@@ -5,14 +5,14 @@ import com.xworkz.parents.internal.ToothPaste;
 public class ToothPasteRunner {
 
     public static void main(String[] args) {
-        ToothPaste toothPaste1=new ToothPaste("Colgate","blue",200,true);
-        ToothPaste toothPaste2=new ToothPaste("Dabur","red",120,false);
-        ToothPaste toothPaste3=new ToothPaste("Colgate","blue",200,true);
+        ToothPaste toothPaste1 = new ToothPaste("Colgate", "blue", 200, true);
+        ToothPaste toothPaste2 = new ToothPaste("Dabur", "red", 120, false);
+        ToothPaste toothPaste3 = new ToothPaste("Colgate", "blue", 200, true);
 
-        boolean match=toothPaste1.equals(toothPaste2);
-        boolean notMatch=toothPaste1.equals(toothPaste3);
+        boolean match = toothPaste1.equals(toothPaste2);
+        boolean notMatch = toothPaste1.equals(toothPaste3);
 
-        System.out.println("match: "+match);
-        System.out.println("notMatch: "+notMatch);
+        System.out.println("match: " + match);
+        System.out.println("notMatch: " + notMatch);
     }
 }
