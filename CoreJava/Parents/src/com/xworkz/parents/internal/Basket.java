d652e6f4ce95084b463fc42862ad5cc21fe50867
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-20 23:52:41 +0530

    overriding equals and toString from Object

--- CoreJava/Parents/src/com/xworkz/parents/internal/Basket.java
+++ CoreJava/Parents/src/com/xworkz/parents/internal/Basket.java
@@ -6,4 +6,20 @@ public class Basket {
     private int capacity;
     private int price;
 
+    public Basket(String color, String material, int capacity, int price){
+        this.capacity=capacity;
+        this.color=color;
+        this.material=material;
+        this.price=price;
+    }
+
+    @Override
+    public String toString() {
+        return "color: " + color + "material: " + material + "capacity: " + capacity + "price: " + price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
