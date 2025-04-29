005e4017eab5668871cb89cf30b350e026f38f66
SonalAcharya911 <sonalacharya911@gmail.com>
2025-04-29 16:01:27 +0530

    string operation

---
+++ CoreJava/Ipl_match_details/src/com/xworks/ipl_match_details/IplMatch.java
@@ -0,0 +1,71 @@
+package com.xworks.ipl_match_details;
+
+import java.util.Arrays;
+
+public class IplMatch {
+
+    public void change(String str){
+        String input=str;
+        String output="";
+
+        String[] words=str.split(" ");
+        for(String word:words){
+            if(word.equals("win")||word.equals("the")){
+                String rev="";
+                System.out.println("==========");
+                for(int i=word.length()-1;i>=0;i--) {
+                    rev = rev + word.charAt(i);
+                }
+                System.out.println(rev);
+                word=rev;
+            }
+            output=output+" "+word;
+
+        }
+        System.out.println("input: "+input);
+        System.out.println("output: "+output);
+    }
+    //rcb will win the match against csk
+    public void letterCount(String str){
+
+        String[] words=str.split(" ");
+        for(String word:words){
+            System.out.print(" "+word.length());
+        }
+
+    }
+
+    public void vowelCounter(String str){
+        int vowel=0;
+        int consonant=0;
+        for(int i=0;i<str.length();i++){
+            char alph=str.charAt(i);
+
+            if (alph=='a'||alph=='e'||alph=='i'||alph=='o'||alph=='u'||alph=='A'||alph=='E'||alph=='I'||alph=='O'||alph=='U'){
+                vowel++;
+            }
+            if(alph=='b'||alph=='c'||alph=='d'||alph=='f'||alph=='g'||alph=='h'||alph=='j'||alph=='k'||alph=='l'||alph=='m'||
+            alph=='n'||alph=='q'||alph=='p'||alph=='r'||alph=='s'||alph=='t'||alph=='y'||alph=='v'||alph=='w'||alph=='x'||alph=='z'||alph=='T'){
+                consonant++;
+            }
+        }
+
+        System.out.println("no of vowels: "+vowel);
+        System.out.println("no of consonants: "+consonant);
+    }
+
+    public void replaceVowel(String str){
+        String temp=str;
+        char[] letters=str.toCharArray();
+        System.out.println(letters);
+        for(char letter:letters){
+            if(letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'){
+                letter= (char) (letter+1);
+            }
+            System.out.print(letter);
+        }
+
+    }
+
+
+}
---
+++ CoreJava/Ipl_match_details/src/com/xworks/ipl_match_details/IplMatchRunner.java
@@ -0,0 +1,15 @@
+package com.xworks.ipl_match_details;
+
+public class IplMatchRunner {
+    public static void main(String[] args) {
+        IplMatch iplMatch=new IplMatch();
+        iplMatch.change("RCB will win the match");
+
+        iplMatch.letterCount("rcb will win the match against csk");
+        System.out.println("\n");
+        iplMatch.vowelCounter("Today is my day, I will do best");
+        System.out.println("\n");
+        iplMatch.replaceVowel("you are welcome");
+        System.out.println("\n");
+    }
+}
