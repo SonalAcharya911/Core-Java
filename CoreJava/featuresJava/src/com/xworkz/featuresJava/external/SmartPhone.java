2dece0d2c93d30589fe9c9eb77eb400066af236b
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-05 23:39:38 +0530

    functional interfaces

--- CoreJava/featuresJava/src/com/xworkz/featuresJava/external/SmartPhone.java
+++ CoreJava/featuresJava/src/com/xworkz/featuresJava/external/SmartPhone.java
@@ -10,8 +10,12 @@ public class SmartPhone implements SmartDevice {
         public void powerOff() {
                 System.out.println("Smartphone powered off");
         }
-        public void connectToWiFi(String network) { System.out.println("Connected to " + network); }
-        public void playMusic(String song) { System.out.println("Playing song: " + song); }
+        public void connectToWiFi(String network) {
+                System.out.println("Connected to " + network);
+        }
+        public void playMusic(String song) {
+                System.out.println("Playing song: " + song);
+        }
         public void takePicture() { System.out.println("Picture taken"); }
         public void browseInternet() { System.out.println("Browsing the internet..."); }
         public void increaseVolume() { System.out.println("Volume increased"); }
