555bef51b73eaa9ef3f09e725ea1cc41df6e8ee7
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-06 23:38:01 +0530

    servlet service method

---
+++ CoreJava/marvel/src/com/xworkz/marvel/servlet/ActionServlet.java
@@ -0,0 +1,21 @@
+package com.xworkz.marvel.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/action",loadOnStartup = 1)
+public class ActionServlet extends GenericServlet {
+
+    public ActionServlet(){
+        System.out.println("ActionServlet created by tomcat");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("running service in ActionServlet");
+    }
+}
---
+++ CoreJava/marvel/src/com/xworkz/marvel/servlet/BaseBallServlet.java
@@ -0,0 +1,4 @@
+package com.xworkz.marvel.servlet;
+
+public class BaseBallServlet {
+}
