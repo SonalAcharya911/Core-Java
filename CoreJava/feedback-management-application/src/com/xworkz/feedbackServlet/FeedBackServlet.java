51cf1d54b168ab28c4caab137b8ea71347475e06
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-07 23:52:25 +0530

    servlet

---
+++ CoreJava/feedback-management-application/src/com/xworkz/feedbackServlet/FeedBackServlet.java
@@ -0,0 +1,32 @@
+package com.xworkz.feedbackServlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/submit")
+public class FeedBackServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("running service in FeedBackServlet");
+        String name=servletRequest.getParameter("userName");
+        String email=servletRequest.getParameter("email");
+        String contact=servletRequest.getParameter("contact");
+        String feedback=servletRequest.getParameter("feedback");
+
+        System.out.println("name: "+name);
+        System.out.println("email: "+email);
+        System.out.println("contact: "+contact);
+        System.out.println("feedback: "+feedback);
+
+        servletResponse.setContentType("text/html");
+        PrintWriter writer= servletResponse.getWriter();
+
+        writer.println("Thanks for submitting the form");
+    }
+}
