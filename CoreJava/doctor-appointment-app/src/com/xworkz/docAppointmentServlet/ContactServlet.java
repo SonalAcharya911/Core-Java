279f313a752fb9f0d01446da4447bbc7fde0a524
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-12 23:14:30 +0530

    doctor appointment app using servlet

---
+++ CoreJava/doctor-appointment-app/src/com/xworkz/docAppointmentServlet/ContactServlet.java
@@ -0,0 +1,27 @@
+package com.xworkz.docAppointmentServlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/getContact", loadOnStartup = 1)
+public class ContactServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        String fname = servletRequest.getParameter("firstname");
+        String lname = servletRequest.getParameter("lastname");
+        String mail = servletRequest.getParameter("email");
+        String message = servletRequest.getParameter("sickness");
+
+        servletResponse.setContentType("text/html");
+        PrintWriter writer = servletResponse.getWriter();
+
+        writer.println("<h1>Contact collected successfully</h1>");
+
+
+    }
+}
