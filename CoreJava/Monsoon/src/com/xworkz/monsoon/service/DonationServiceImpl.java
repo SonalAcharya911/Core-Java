3980412ffbf1d52d42a0b4da397faf9379362524
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-20 23:43:20 +0530

    product application

---
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/service/DonationServiceImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.monsoon.service;
+
+import com.xworkz.monsoon.dto.DonationDto;
+
+public class DonationServiceImpl implements DonationService{
+    @Override
+    public boolean save(DonationDto donationDto) {
+        return false;
+    }
+}
