a243dca704a6337ecd3b4f7bf09fce20239d79fa
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-20 23:43:01 +0530

    product application

---
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/dto/DonationDto.java
@@ -0,0 +1,23 @@
+package com.xworkz.monsoon.dto;
+
+public class DonationDto {
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
