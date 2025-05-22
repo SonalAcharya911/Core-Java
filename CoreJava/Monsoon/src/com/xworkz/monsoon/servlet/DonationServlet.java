293b985a988ca7aaa133a1b365e05a80be61634c
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-22 23:58:59 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/DonationServlet.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/DonationServlet.java
@@ -1,5 +1,7 @@
 package com.xworkz.monsoon.servlet;
 
+import com.xworkz.monsoon.dto.DonationDto;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +18,29 @@ public class DonationServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("running doPost in DonationServlet");
+        String name=req.getParameter("name");
+        String cont=req.getParameter("contact");
+        long contact=0;
+        if(cont!=null || cont!=""){
+            contact=Long.parseLong(cont);
+        }
+
+        String email=req.getParameter("email");
+
+        String amt=req.getParameter("amount");
+        double amount=0;
+        if(amt!=null || amt!=""){
+            amount=Double.parseDouble(amt);
+        }
+        String description=req.getParameter("desc");
+
+        DonationDto donationDto=new DonationDto();
+        donationDto.setAmount(amount);
+        donationDto.setName(name);
+        donationDto.setContact(contact);
+        donationDto.setEmail(email);
+        donationDto.setDescription(description);
+
 
 
     }
