f307e247d83e70ab975df06adfcb6171bc649310
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-22 23:58:54 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/service/DonationServiceImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/service/DonationServiceImpl.java
@@ -13,6 +13,21 @@ public class DonationServiceImpl implements DonationService{
         System.out.println("running save in DonationServiceImpl...");
         if(donationDto!=null){
             System.out.println("donationDto is not null");
+            if(donationDto.getName()!=null && donationDto.getName().length()>=3){
+                System.out.println("name is valid");
+            }
+            else{
+                System.out.println("name is not valid");
+                return false;
+            }
+            if (donationDto.getContact()!=0){
+                System.out.println("valid contact");
+            }
+            else{
+                System.out.println("contact is not valid");
+                return false;
+            }
+
         }
         else{
             System.out.println("donationDto is cannot be null");
