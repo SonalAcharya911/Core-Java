f1563cdd89d190667a185209b46905b690f4b098
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-22 23:16:03 +0530

    donation service implementation

--- CoreJava/Monsoon/src/com/xworkz/monsoon/service/DonationServiceImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/service/DonationServiceImpl.java
@@ -1,10 +1,23 @@
 package com.xworkz.monsoon.service;
 
 import com.xworkz.monsoon.dto.DonationDto;
+import com.xworkz.monsoon.servlet.DonationServlet;
 
 public class DonationServiceImpl implements DonationService{
+
+    public DonationServiceImpl(){
+        System.out.println("DonationServiceImpl created...");
+    }
     @Override
     public boolean save(DonationDto donationDto) {
-        return false;
+        System.out.println("running save in DonationServiceImpl...");
+        if(donationDto!=null){
+            System.out.println("donationDto is not null");
+        }
+        else{
+            System.out.println("donationDto is cannot be null");
+            return false;
+        }
+        return true;
     }
 }
