49ae4e79c8559d8145a21489c43cce5651db92de
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-22 23:15:48 +0530

    donation repo

--- CoreJava/Monsoon/src/com/xworkz/monsoon/repository/DonationRepositoryImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/repository/DonationRepositoryImpl.java
@@ -6,7 +6,13 @@ public class DonationRepositoryImpl implements DonationRepository{
     @Override
     public boolean persist(DonationDto donationDto) {
         System.out.println("running persist in DonationRepositoryImpl");
-
-        return false;
+        if(donationDto!=null){
+            System.out.println("donationDto is not null");
+        }
+        else{
+            System.out.println("donationDto is cannot be null");
+            return false;
+        }
+        return true;
     }
 }
