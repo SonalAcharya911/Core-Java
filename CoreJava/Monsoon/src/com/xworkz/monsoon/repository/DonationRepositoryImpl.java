5113eb2ec0369817b9d606bec5f5497990bc08ea
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-22 13:47:46 +0530

    Donation App

--- CoreJava/Monsoon/src/com/xworkz/monsoon/repository/DonationRepositoryImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/repository/DonationRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.xworkz.monsoon.dto.DonationDto;
 public class DonationRepositoryImpl implements DonationRepository{
     @Override
     public boolean persist(DonationDto donationDto) {
+        System.out.println("running persist in DonationRepositoryImpl");
+
         return false;
     }
 }
