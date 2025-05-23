1f9940d15cc4a10e5b7c414cc59af9fc36621655
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-23 16:12:38 +0530

    Recipe Ingredients app

---
+++ CoreJava/recipe-ingredients/src/com/xworkz/recipe/service/RecipeServiceImpl.java
@@ -0,0 +1,55 @@
+package com.xworkz.recipe.service;
+
+import com.xworkz.recipe.dto.RecipeDto;
+
+public class RecipeServiceImpl implements RecipeService{
+    @Override
+    public boolean save(RecipeDto recipeDto) {
+        if(recipeDto!=null ){
+            System.out.println("recipeDto is not null");
+            if(recipeDto.getRecipeName()!=null && recipeDto.getRecipeName().length()>=4){
+                System.out.println("valid recipe name");
+            }else{
+                System.out.println("name not valid...should be at least 4 letters");
+                return false;
+            }
+            if(recipeDto.getIng1()!=null){
+                System.out.println("valid ingredient");
+            } else {
+                System.out.println("invalid ingredient");
+                return false;
+            }
+            if(recipeDto.getIng2()!=null){
+                System.out.println("valid ingredient");
+            } else {
+                System.out.println("invalid ingredient");
+                return false;
+            }
+            if(recipeDto.getIng3()!=null){
+                System.out.println("valid ingredient");
+            } else {
+                System.out.println("invalid ingredient");
+                return false;
+            }
+            if(recipeDto.getIng4()!=null){
+                System.out.println("valid ingredient");
+            } else {
+                System.out.println("invalid ingredient");
+                return false;
+            }
+            if(recipeDto.getIng5()!=null){
+                System.out.println("valid ingredient");
+            } else {
+                System.out.println("invalid ingredient");
+                return false;
+            }
+        }
+
+        else{
+            System.out.println("RecipeDto is null");
+            return false;
+
+        }
+        return true;
+    }
+}
---
+++ CoreJava/recipe-ingredients/src/com/xworkz/recipe/servlet/RecipeServlet.java
@@ -0,0 +1,56 @@
+package com.xworkz.recipe.servlet;
+
+import com.xworkz.recipe.dto.RecipeDto;
+import com.xworkz.recipe.repository.RecipeRepository;
+import com.xworkz.recipe.repository.RecipeRepositoryImpl;
+import com.xworkz.recipe.service.RecipeService;
+import com.xworkz.recipe.service.RecipeServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/getRecipe")
+public class RecipeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name=req.getParameter("name");
+        String ing1=req.getParameter("ing1");
+        String ing2=req.getParameter("ing2");
+        String ing3=req.getParameter("ing3");
+        String ing4=req.getParameter("ing4");
+        String ing5=req.getParameter("ing5");
+
+        RecipeDto recipeDto=new RecipeDto();
+        recipeDto.setRecipeName(name);
+        recipeDto.setIng1(ing1);
+        recipeDto.setIng2(ing2);
+        recipeDto.setIng3(ing3);
+        recipeDto.setIng4(ing4);
+        recipeDto.setIng5(ing5);
+
+        RecipeService recipeService=new RecipeServiceImpl();
+        boolean saved=recipeService.save(recipeDto);
+        if(saved){
+            System.out.println("data is valid");
+            RequestDispatcher requestDispatcher= req.getRequestDispatcher("recipeSuccess.jsp");
+            req.setAttribute("recipeDto",recipeDto);
+            requestDispatcher.forward(req,resp);
+        }
+        else{
+            System.out.println("data id invalid");
+            RequestDispatcher requestDispatcher=req.getRequestDispatcher("recipe.jsp");
+            req.setAttribute("message","saving failed");
+            requestDispatcher.forward(req,resp);
+        }
+
+
+
+
+
+    }
+}
