fa4b49e32fa95864b9a212dcc3c0f8ed598c5d6f
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 00:04:45 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/service/LaboratoryServiceImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/service/LaboratoryServiceImpl.java
@@ -4,6 +4,10 @@ import com.xworkz.monsoon.dto.LaboratoryDto;
 
 public class LaboratoryServiceImpl implements LaboratoryService
 {
+    public LaboratoryServiceImpl(){
+        System.out.println("LaboratoryServiceImpl created...");
+    }
+
     @Override
     public boolean save(LaboratoryDto laboratoryDto) {
         return false;
--- CoreJava/Monsoon/src/com/xworkz/monsoon/service/LicenseServiceImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/service/LicenseServiceImpl.java
@@ -3,6 +3,10 @@ package com.xworkz.monsoon.service;
 import com.xworkz.monsoon.dto.LicenseDto;
 
 public class LicenseServiceImpl implements LicenseService{
+    public LicenseServiceImpl(){
+        System.out.println("LicenseServiceImpl created...");
+    }
+
     @Override
     public boolean save(LicenseDto licenseDto) {
         return false;
