2f4ec8443c0609f07337aa5b72b5e36cdb2148dc
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 00:02:03 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/repository/LicenseRepoImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/repository/LicenseRepoImpl.java
@@ -5,7 +5,15 @@ import com.xworkz.monsoon.dto.LicenseDto;
 public class LicenseRepoImpl implements LicenseRepo{
     @Override
     public boolean persist(LicenseDto licenseDto) {
-        return false;
+        System.out.println("running persist in LicenseRepoImpl");
+        if(licenseDto!=null){
+            System.out.println("licenseDto is not null");
+        }
+        else{
+            System.out.println("licenseDto is cannot be null");
+            return false;
+        }
+        return true;
     }
 
 }
