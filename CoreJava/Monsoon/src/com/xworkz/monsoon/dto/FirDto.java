c4ce86b20e504d9057fb00bad04f49be00eeecb0
SonalAcharya911 <sonalacharya911@gmail.com>
2025-05-31 23:54:08 +0530

    app servlet

---
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/dto/FirDto.java
@@ -0,0 +1,59 @@
+package com.xworkz.monsoon.dto;
+
+public class FirDto {
+    String name;
+    String address;
+    String contact;
+    String date;
+    String area;
+    String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
