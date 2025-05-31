e3e38e27d057f768c2295555413f3202c4a8faa7
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 00:02:27 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/repository/LaboratoryRepoImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/repository/LaboratoryRepoImpl.java
@@ -5,6 +5,14 @@ import com.xworkz.monsoon.dto.LaboratoryDto;
 public class LaboratoryRepoImpl implements LaboratoryRepo{
     @Override
     public boolean persist(LaboratoryDto laboratoryDto) {
-        return false;
+        System.out.println("running persist in LaboratoryRepoImpl");
+        if(laboratoryDto!=null){
+            System.out.println("laboratoryDto is not null");
+        }
+        else{
+            System.out.println("laboratoryDto is cannot be null");
+            return false;
+        }
+        return true;
     }
 }
