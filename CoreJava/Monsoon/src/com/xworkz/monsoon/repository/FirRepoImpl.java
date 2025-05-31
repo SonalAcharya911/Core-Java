f5ca7f7fa4dd0cd3f58ca84da5526bdc4cc2b6a7
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 00:02:19 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/repository/FirRepoImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/repository/FirRepoImpl.java
@@ -5,6 +5,14 @@ import com.xworkz.monsoon.dto.FirDto;
 public class FirRepoImpl implements FirRepo{
     @Override
     public boolean persist(FirDto firDto) {
-        return false;
+        System.out.println("running persist in FirRepoImpl");
+        if(firDto!=null){
+            System.out.println("firDto is not null");
+        }
+        else{
+            System.out.println("firDto is cannot be null");
+            return false;
+        }
+        return true;
     }
 }
