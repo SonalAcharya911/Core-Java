684106d168c7c46c667209741db997a867149002
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 00:04:37 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/service/FirServiceImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/service/FirServiceImpl.java
@@ -3,6 +3,10 @@ package com.xworkz.monsoon.service;
 import com.xworkz.monsoon.dto.FirDto;
 
 public class FirServiceImpl implements FirService{
+    public FirServiceImpl(){
+        System.out.println("FirServiceImpl created...");
+    }
+
     @Override
     public boolean save(FirDto firDto) {
         return false;
