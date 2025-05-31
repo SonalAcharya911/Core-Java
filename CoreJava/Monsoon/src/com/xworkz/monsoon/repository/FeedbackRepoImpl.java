9871973607f2bd02a441662700b47deb71187880
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 00:02:12 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/repository/FeedbackRepoImpl.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/repository/FeedbackRepoImpl.java
@@ -5,6 +5,14 @@ import com.xworkz.monsoon.dto.FeedbackDto;
 public class FeedbackRepoImpl implements FeedbackRepo{
     @Override
     public boolean persist(FeedbackDto feedbackDto) {
-        return false;
+        System.out.println("running persist in FeedbackRepoImpl");
+        if(feedbackDto!=null){
+            System.out.println("feedbackDto is not null");
+        }
+        else{
+            System.out.println("feedbackDto is cannot be null");
+            return false;
+        }
+        return true;
     }
 }
