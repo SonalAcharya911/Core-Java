8a094e3b2ab6c3cbf8579eba405d3a59c512960d
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 23:56:21 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/LaboratoryServlet.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/LaboratoryServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LaboratoryServlet extends HttpServlet {
+
+    public LaboratoryServlet(){
+        System.out.println("LaboratoryServlet created...");
+    }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
