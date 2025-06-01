f1b4faca7646abc729b53cbe8ec6997326706a83
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-01 23:56:28 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/FIRServlet.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/FIRServlet.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FIRServlet extends HttpServlet {
+
+    public FIRServlet(){
+        System.out.println("FIRServlet created....");
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
