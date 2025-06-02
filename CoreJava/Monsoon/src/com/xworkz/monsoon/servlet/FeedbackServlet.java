f98041c4402e08e0bd4f4a63ae0bd273ca11d61d
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-02 22:58:10 +0530

    app servlet

--- CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/FeedbackServlet.java
+++ CoreJava/Monsoon/src/com/xworkz/monsoon/servlet/FeedbackServlet.java
@@ -1,17 +1,28 @@
 package com.xworkz.monsoon.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/getFeedback")
 public class FeedbackServlet extends HttpServlet {
     public FeedbackServlet(){
         System.out.println("FeedbackServlet created....");
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String name= req.getParameter("name");
+        String userAge=req.getParameter("age");
+        String feedback=req.getParameter("feedback");
+        String subject=req.getParameter("subject");
+        int age=0;
+        if(userAge!=null){
+            age=Integer.parseInt(userAge);
+        }
+
+
     }
 }
