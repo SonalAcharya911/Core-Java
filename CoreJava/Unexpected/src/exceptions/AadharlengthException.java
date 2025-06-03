7d053426d072ecbb08ef697272699a89044581b5
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-03 16:25:25 +0530

    Manual Exception

---
+++ CoreJava/Unexpected/src/exceptions/AadharlengthException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class AadharlengthException extends RuntimeException{
+    public AadharlengthException(String message){
+        super(message);
+
+    }
+}
---
+++ CoreJava/Unexpected/src/exceptions/ContactLengthexception.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class ContactLengthexception extends RuntimeException{
+    public ContactLengthexception(String message){
+        super(message);
+    }
+}
