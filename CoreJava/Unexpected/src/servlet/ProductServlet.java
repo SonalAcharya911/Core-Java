9991236c2e24ca8c577b276a00cb1fa1007b3f23
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-04 23:59:39 +0530

    Manual Exception

---
+++ CoreJava/Unexpected/src/servlet/ProductServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ProductServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name=req.getParameter("");
+        String prodID;
+        String category;
+        String price;
+        String quantity;
+
+    }
+}
