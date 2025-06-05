cbae172c4933b362b6f62626e3e4f7fe0281b75f
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-05 23:42:56 +0530

    Exceptions

--- CoreJava/Unexpected/src/service/ProductServiceImpl.java
+++ CoreJava/Unexpected/src/service/ProductServiceImpl.java
@@ -1,8 +1,65 @@
 package service;
 
+import dto.ProductDto;
+import exceptions.*;
+
 public class ProductServiceImpl implements ProductService{
     @Override
-    public boolean save() {
+    public boolean save(ProductDto productDto) {
+        String prodName= productDto.getProductName();
+        String category= productDto.getCategory();
+        Integer quantity=productDto.getQuantity();
+        Integer prodID= productDto.getProductID();
+        Double price=productDto.getPrice();;
+
+        if(productDto.getProductName()!=null){
+            throw new NullProductNameException("product name cannot be empty");
+        }
+        else{
+            System.out.println("valid name");
+        }
+
+        if(prodName.length()<3){
+            throw new ProductNameLengthException("product name should be longer than 3 characters");
+        }
+        else{
+            System.out.println("valid name");
+
+        }
+
+        if(prodID<0){
+            throw new ProductIDNotPositiveException("product ID should be a positive integer");
+        }
+        else{
+            System.out.println("valid ID");
+
+        }
+
+        if(price<0 || price>10000){
+            throw new PriceLimitException("price should be within the range 0-10000");
+        }
+        else{
+            System.out.println("valid price");
+
+        }
+
+        if(quantity<0 || quantity>1000){
+            throw new QuantityLimitException("quantity should be in the range 0-1000");
+        }
+        else{
+            System.out.println("valid quantity");
+
+        }
+
+        if(!(category.equals("Electronics")|| category.equals("Clothing") || category.equals("Books")||category.equals("Home"))){
+            throw new WrongCategoryException("category doesn't match");
+        }
+        else{
+            System.out.println("valid category");
+
+        }
+
         return true;
+
     }
 }
--- CoreJava/Unexpected/src/servlet/ProductServlet.java
+++ CoreJava/Unexpected/src/servlet/ProductServlet.java
@@ -1,5 +1,7 @@
 package servlet;
 
+import dto.ProductDto;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +11,19 @@ import java.io.IOException;
 public class ProductServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name=req.getParameter("");
-        String prodID;
-        String category;
-        String price;
-        String quantity;
+        String name=req.getParameter("productName");
+        String prodID=req.getParameter("productID");
+        String category=req.getParameter("category");
+        String amount=req.getParameter("price");
+        String prodQuantity=req.getParameter("quantity");
+
+        Integer productID=Integer.parseInt(prodID);
+        Double price=Double.parseDouble(amount);
+        Integer quantity=Integer.parseInt(prodQuantity);
+
+        ProductDto productDto=new ProductDto();
+
+
 
     }
 }
