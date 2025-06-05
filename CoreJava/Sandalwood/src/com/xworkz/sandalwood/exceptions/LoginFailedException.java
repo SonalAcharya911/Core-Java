8f5f1a871c7ae9e6affdd9e74608b0d7a6009ff3
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-05 23:40:00 +0530

    Exceptions

---
+++ CoreJava/Sandalwood/src/com/xworkz/sandalwood/exceptions/LoginFailedException.java
@@ -0,0 +1,8 @@
+package com.xworkz.sandalwood.exceptions;
+
+public class LoginFailedException extends Exception{
+    public LoginFailedException(String message){
+        super(message);
+    }
+}
+
