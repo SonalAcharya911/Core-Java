ba02117e1c7c158d8c5e8d2ecd1e2c58e22b5b70
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-06 23:54:03 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/runner/DevisionCalculatorRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.extraordinary.runner;
+
+import com.xworkz.extraordinary.dto.DevisionCalculatorDto;
+
+import java.util.Scanner;
+
+public class DevisionCalculatorRunner {
+    public static void main(String[] args) {
+        DevisionCalculatorDto devisionCalculatorDto=new DevisionCalculatorDto();
+
+        try(Scanner scanner=new Scanner(System.in)){
+
+        }
+    }
+}
