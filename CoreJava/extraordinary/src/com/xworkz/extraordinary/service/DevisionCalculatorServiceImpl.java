b1f08ec23ce97f1d7b15ecc2c058d3eacb86b475
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-06 23:54:13 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/service/DevisionCalculatorServiceImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.extraordinary.service;
+
+import com.xworkz.extraordinary.dto.DevisionCalculatorDto;
+
+public class DevisionCalculatorServiceImpl implements DevisionCalculatorService{
+    @Override
+    public boolean validate(DevisionCalculatorDto devisionCalculatorDto) {
+        return false;
+    }
+}
