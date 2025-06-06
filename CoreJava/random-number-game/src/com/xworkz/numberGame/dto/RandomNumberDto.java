5c25c0c033d81dfcf966390fb4fad51268b91493
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-06 23:45:05 +0530

    Exceptions

---
+++ CoreJava/random-number-game/src/com/xworkz/numberGame/dto/RandomNumberDto.java
@@ -0,0 +1,17 @@
+package com.xworkz.numberGame.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Scanner;
+
+@Getter
+@Setter
+
+
+public class RandomNumberDto {
+
+    private int genNum;
+    private int guessedNumber;
+
+}
