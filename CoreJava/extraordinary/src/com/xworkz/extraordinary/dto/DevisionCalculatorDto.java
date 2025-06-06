821fca92831028493fa10fe53fafde88d0c0fd0e
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-06 23:53:58 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/dto/DevisionCalculatorDto.java
@@ -0,0 +1,8 @@
+package com.xworkz.extraordinary.dto;
+
+public class DevisionCalculatorDto {
+    private double divider;
+    private double divident;
+    private double quotient;
+    private double remainder;
+}
