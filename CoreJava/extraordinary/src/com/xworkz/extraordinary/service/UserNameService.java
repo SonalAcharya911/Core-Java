9868cb94db767cba6589658c4dc26aa03dde1a99
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-07 23:57:35 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/service/UserNameService.java
@@ -0,0 +1,7 @@
+package com.xworkz.extraordinary.service;
+
+import com.xworkz.extraordinary.dto.UserNameDto;
+
+public interface UserNameService {
+    boolean validate(UserNameDto userNameDto);
+}
