ef4147399156da5abb8aa435399dec0b9b8e78d0
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-07 23:57:56 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/dto/UserNameDto.java
@@ -0,0 +1,11 @@
+package com.xworkz.extraordinary.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserNameDto {
+    private String userName;
+    private String nickName;
+}
