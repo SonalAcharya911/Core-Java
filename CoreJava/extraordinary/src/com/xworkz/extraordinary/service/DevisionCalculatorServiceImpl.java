96c15bd394e5e1834ed9987a3ff811f634e35a37
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-07 23:39:53 +0530

    Exceptions task

--- CoreJava/extraordinary/src/com/xworkz/extraordinary/service/DevisionCalculatorServiceImpl.java
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/service/DevisionCalculatorServiceImpl.java
@@ -5,6 +5,22 @@ import com.xworkz.extraordinary.dto.DevisionCalculatorDto;
 public class DevisionCalculatorServiceImpl implements DevisionCalculatorService{
     @Override
     public boolean validate(DevisionCalculatorDto devisionCalculatorDto) {
-        return false;
+        try{
+            if(devisionCalculatorDto!=null){
+                if(devisionCalculatorDto.getDivisor()!=0){
+                    System.out.println("valid divisor");
+                }else{
+                    throw new ArithmeticException("divide by zero not possible");
+                }
+            }
+            else{
+                throw new NullPointerException("dto is null");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return true;
     }
 }
