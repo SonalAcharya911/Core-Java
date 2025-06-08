59032e211699f1acfe4048f1be01d647d5dc60d5
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-08 23:47:39 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/service/IngredientsService.java
@@ -0,0 +1,7 @@
+package com.xworkz.extraordinary.service;
+
+import com.xworkz.extraordinary.dto.IngredientsDto;
+
+public interface IngredientsService {
+    boolean validate(IngredientsDto ingredientsDto);
+}
