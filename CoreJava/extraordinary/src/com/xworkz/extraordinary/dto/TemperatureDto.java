caa3f0f8bde7ae6f18977c941e1bcdb8c6060e03
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-08 23:47:49 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/dto/TemperatureDto.java
@@ -0,0 +1,12 @@
+package com.xworkz.extraordinary.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TemperatureDto {
+    private double temperature;
+}
