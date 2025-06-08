bed4eb735e6f002412374798e82ac1c40ef0bb32
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-08 23:47:17 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/dto/IngredientsDto.java
@@ -0,0 +1,12 @@
+package com.xworkz.extraordinary.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class IngredientsDto {
+    private int size;
+    private String[] igredients;
+    private int index;
+}
