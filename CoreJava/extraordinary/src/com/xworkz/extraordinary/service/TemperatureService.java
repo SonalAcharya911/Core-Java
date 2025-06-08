cd698f3506b207378cb98e07a074f8182aea70e1
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-08 23:48:11 +0530

    Exceptions task

---
+++ CoreJava/extraordinary/src/com/xworkz/extraordinary/service/TemperatureService.java
@@ -0,0 +1,7 @@
+package com.xworkz.extraordinary.service;
+
+import com.xworkz.extraordinary.dto.TemperatureDto;
+
+public interface TemperatureService {
+    boolean validate(TemperatureDto temperatureDto);
+}
