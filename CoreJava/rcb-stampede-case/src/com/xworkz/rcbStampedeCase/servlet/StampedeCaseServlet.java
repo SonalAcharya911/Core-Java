d948bc67d538f4f7013a78ba9897a9bdb67b7e6b
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-10 23:37:07 +0530

    connection to db

---
+++ CoreJava/rcb-stampede-case/src/com/xworkz/rcbStampedeCase/servlet/StampedeCaseServlet.java
@@ -0,0 +1,28 @@
+package com.xworkz.rcbStampedeCase.servlet;
+
+import com.xworkz.rcbStampedeCase.dto.StampedeCaseDto;
+import com.xworkz.rcbStampedeCase.service.StampedeCaseService;
+import com.xworkz.rcbStampedeCase.service.StampedeCaseServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class StampedeCaseServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String event=req.getParameter("event");
+        String location=req.getParameter("locatiion");
+        String deaths=req.getParameter("deaths");
+        String injuries=req.getParameter("injuries");
+
+        StampedeCaseDto stampedeCaseDto=new StampedeCaseDto(event,location,Integer.parseInt(deaths),Integer.parseInt(injuries));
+
+        StampedeCaseService stampedeCaseService=new StampedeCaseServiceImpl();
+        String result=stampedeCaseService.save(stampedeCaseDto);
+
+        System.out.println("result : "+result);
+    }
+}
