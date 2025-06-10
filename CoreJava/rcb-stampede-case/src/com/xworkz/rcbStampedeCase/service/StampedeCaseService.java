b6563c201e73286f5bc7356c155219c93357f5e9
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-10 23:37:16 +0530

    connection to db

---
+++ CoreJava/rcb-stampede-case/src/com/xworkz/rcbStampedeCase/service/StampedeCaseService.java
@@ -0,0 +1,7 @@
+package com.xworkz.rcbStampedeCase.service;
+
+import com.xworkz.rcbStampedeCase.dto.StampedeCaseDto;
+
+public interface StampedeCaseService {
+    String save(StampedeCaseDto stampedeCaseDto);
+}
