d299d6ef3c935d6f200d25258196f3b49cbbf344
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-11 23:57:56 +0530

    jdbc

---
+++ CoreJava/weather/src/com/xworkz/weather/service/WeatherServiceImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.weather.service;
+
+import com.xworkz.weather.dto.WeatherDto;
+
+public class WeatherServiceImpl implements WeatherService{
+    @Override
+    public boolean validate(WeatherDto weatherDto) {
+        return false;
+    }
+}
