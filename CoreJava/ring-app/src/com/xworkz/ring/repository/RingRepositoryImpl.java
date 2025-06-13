debeda041a0ba02c31b0df5e0faf57281bd5dc74
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-13 23:29:21 +0530

    ring app

--- CoreJava/ring-app/src/com/xworkz/ring/repository/RingRepositoryImpl.java
+++ CoreJava/ring-app/src/com/xworkz/ring/repository/RingRepositoryImpl.java
@@ -1,10 +1,38 @@
 package com.xworkz.ring.repository;
 
 import com.xworkz.ring.dto.RingDto;
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class RingRepositoryImpl implements RingRepository{
+    @SneakyThrows
     @Override
     public boolean persist(RingDto ringDto) {
+        System.out.println("running persist in RingRepositoryImpl");
+        if(ringDto!=null){
+            System.out.println("dto is not null....should save into repo");
+            String url="jdbc:mysql://localhost:3306/weather";
+            String password="Sonal@7715";
+            String username="root";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try(Connection connection= DriverManager.getConnection(url,username,password)){
+                String sql="INSERT INTO ring_data(material,weight,size,price,purchase_date,have_stones,custom_made) VALUES ('"+ ringDto.getMaterial()
+                        +"',"+ ringDto.getWeight()+","+ ringDto.getSize()+","+ ringDto.getPrice()+",'"+ ringDto.getPurchaseDate()+"',"+ringDto.isHaveStones()+","+ringDto.isCustomMade()+");";
+                Statement statement=connection.createStatement();
+                statement.executeUpdate(sql);
+
+            }
+            catch(SQLException e){
+                System.out.println(e.getMessage());
+                return false;
+            }
+            return true;
+        }
         return false;
     }
 }
