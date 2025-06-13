662a9f3fbbe013cb5a58230567d2243cd84f2a95
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-13 23:28:57 +0530

    ring app

---
+++ CoreJava/ring-app/src/com/xworkz/ring/servlet/RingServlet.java
@@ -0,0 +1,45 @@
+package com.xworkz.ring.servlet;
+
+import com.xworkz.ring.dto.RingDto;
+import com.xworkz.ring.service.RingService;
+import com.xworkz.ring.service.RingServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet(urlPatterns = "/ring")
+public class RingServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String material=req.getParameter("material");
+        String weight=req.getParameter("weight");
+        String size=req.getParameter("size");
+        String price=req.getParameter("price");
+        String purchaseDate=req.getParameter("purchaseDate");
+        String haveStones=req.getParameter("haveStones");
+        String customMade=req.getParameter("customMade");
+
+        System.out.println("haveStones: "+haveStones+".....customMade: "+customMade);
+        RingDto ringDto=new RingDto(material,Long.parseLong(weight),Integer.parseInt(size),Long.parseLong(price), LocalDate.parse(purchaseDate),Boolean.parseBoolean(haveStones),Boolean.parseBoolean(customMade));
+
+        RingService ringService=new RingServiceImpl();
+        boolean valid=ringService.save(ringDto);
+
+        if(valid){
+            RequestDispatcher requestDispatcher=req.getRequestDispatcher("ring.jsp");
+            req.setAttribute("message","saved successfully");
+            requestDispatcher.forward(req,resp);
+        }
+        else{
+            RequestDispatcher requestDispatcher=req.getRequestDispatcher("ring.jsp");
+
+        }
+    }
+}
