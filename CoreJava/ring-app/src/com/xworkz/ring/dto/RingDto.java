60fe5a48e2c8512bd785b31324f977182d2747d8
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-13 23:29:27 +0530

    ring app

--- CoreJava/ring-app/src/com/xworkz/ring/dto/RingDto.java
+++ CoreJava/ring-app/src/com/xworkz/ring/dto/RingDto.java
@@ -1,4 +1,21 @@
 package com.xworkz.ring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class RingDto {
+    private String material;
+    private long weight;
+    private int size;
+    private long price;
+    private LocalDate purchaseDate;
+    private boolean haveStones;
+    private boolean customMade;
 }
