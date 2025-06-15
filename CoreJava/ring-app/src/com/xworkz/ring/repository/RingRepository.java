d8b055ae90932c9c3fb5a7fcc66ecb864f2fce32
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-15 23:59:51 +0530

    ring app

--- CoreJava/ring-app/src/com/xworkz/ring/repository/RingRepository.java
+++ CoreJava/ring-app/src/com/xworkz/ring/repository/RingRepository.java
@@ -4,7 +4,5 @@ import com.xworkz.ring.dto.RingDto;
 
 public interface RingRepository {
     boolean persist(RingDto ringDto);
-    default void save(){
-        System.out.println("jdhg");
-    }
+    
 }
