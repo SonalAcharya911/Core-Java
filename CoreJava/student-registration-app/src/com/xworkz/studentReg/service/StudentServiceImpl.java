a1d3d34a5fab551fc9e1b6523efa23d0af92a0a2
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-20 23:07:32 +0530

    birthday party manager app

--- CoreJava/student-registration-app/src/com/xworkz/studentReg/service/StudentServiceImpl.java
+++ CoreJava/student-registration-app/src/com/xworkz/studentReg/service/StudentServiceImpl.java
@@ -6,8 +6,13 @@ public class StudentServiceImpl implements StudentService{
     @Override
     public void printStudentDetails(StudentDto studentDto) {
         System.out.println("studentName: "+studentDto.getStudentName());
-        System.out.println("student Roll Number: "+studentDto.getRollNumber());
+        System.out.println("student Course: "+studentDto.getCourse());
         System.out.println("student email: "+studentDto.getEmail());
 
+    }
+
+    @Override
+    public boolean save(StudentDto studentDto) {
+        return false;
     }
 }
