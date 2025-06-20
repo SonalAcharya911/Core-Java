b609ecaa6c583dd21c05999f30cb4adabf926821
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-20 23:07:18 +0530

    birthday party manager app

--- CoreJava/student-registration-app/src/com/xworkz/studentReg/repo/StudentRepositoryImpl.java
+++ CoreJava/student-registration-app/src/com/xworkz/studentReg/repo/StudentRepositoryImpl.java
@@ -2,10 +2,8 @@ package com.xworkz.studentReg.repo;
 
 import com.xworkz.studentReg.dto.StudentDto;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Optional;
 
 public class StudentRepositoryImpl implements StudentRepository{
     @Override
@@ -35,6 +33,36 @@ public class StudentRepositoryImpl implements StudentRepository{
 
 
 
+
+    }
+
+    @Override
+    public Optional<StudentDto> findById(int id) {
+        StudentDto studentDto=new StudentDto();
+        String url="jdbc:mysql://localhost:3306/student";
+        String userName="root";
+        String password="example@1234";
+        if(id!=0){
+
+            String findByIdQuery="SELECT * FROM student s WHERE s.id=?";
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                try(Connection connection=DriverManager.getConnection(url,userName,password);
+                    PreparedStatement preparedStatement=connection.prepareStatement(findByIdQuery)){
+                    preparedStatement.setInt(1,id);
+
+                    ResultSet resultSet=preparedStatement.executeQuery();
+
+                    studentDto.setStudentName(resultSet.getString(2));
+                    studentDto.setEmail(resultSet.getString(3));
+                    studentDto.setCourse(resultSet.getString(4));
+                }
+            } catch (ClassNotFoundException|SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return Optional.of(studentDto);
+        }
+        return Optional.empty();
 
     }
 }
--- CoreJava/student-registration-app/src/com/xworkz/studentReg/service/StudentService.java
+++ CoreJava/student-registration-app/src/com/xworkz/studentReg/service/StudentService.java
@@ -4,4 +4,6 @@ import com.xworkz.studentReg.dto.StudentDto;
 
 public interface StudentService {
     void printStudentDetails(StudentDto studentDto);
+
+    boolean save(StudentDto studentDto);
 }
