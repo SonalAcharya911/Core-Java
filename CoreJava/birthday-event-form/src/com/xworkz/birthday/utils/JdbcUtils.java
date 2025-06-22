434710deebdacd127b7433cc1aae3184b75d84eb
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-22 23:51:09 +0530

    working on jdbc..

--- CoreJava/birthday-event-form/src/com/xworkz/birthday/utils/JdbcUtils.java
+++ CoreJava/birthday-event-form/src/com/xworkz/birthday/utils/JdbcUtils.java
@@ -3,8 +3,6 @@ package com.xworkz.birthday.utils;
 public class JdbcUtils {
     public JdbcUtils(){
         System.out.println("no args contructor of JdbcUtils");
-        System.out.println();
-        System.err.println();
     }
 
     public final static String URL ="jdbc:mysql://localhost:3306/weather";
