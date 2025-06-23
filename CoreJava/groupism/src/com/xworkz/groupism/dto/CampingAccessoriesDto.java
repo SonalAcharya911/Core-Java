12c38548ddfd0024538feac18803c8cde8016a8f
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-24 01:57:54 +0530

    working with collections

--- CoreJava/groupism/src/com/xworkz/groupism/dto/CampingAccessoriesDto.java
+++ CoreJava/groupism/src/com/xworkz/groupism/dto/CampingAccessoriesDto.java
@@ -1,4 +1,17 @@
 package com.xworkz.groupism.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+
 public class CampingAccessoriesDto {
+    private String name;
+    private String brand;
+    private double weight;
+    private double price;
+    private String suitableWeather;
 }
