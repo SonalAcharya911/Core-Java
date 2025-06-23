b59fb7edee7dd0612f35881280c01415d432d1c6
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-24 01:58:24 +0530

    working with collections

--- CoreJava/groupism/src/com/xworkz/groupism/dto/SareeDto.java
+++ CoreJava/groupism/src/com/xworkz/groupism/dto/SareeDto.java
@@ -1,4 +1,16 @@
 package com.xworkz.groupism.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class SareeDto {
+    private String occasion;
+    private String material;
+    private double cost;
+    private String color;
+    private boolean withBlousePiece;
 }
