37b54135a305acbc3483db1d55ffb2fbcbc665cf
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-24 01:58:07 +0530

    working with collections

--- CoreJava/groupism/src/com/xworkz/groupism/dto/FurnitureDto.java
+++ CoreJava/groupism/src/com/xworkz/groupism/dto/FurnitureDto.java
@@ -1,4 +1,17 @@
 package com.xworkz.groupism.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class FurnitureDto {
+    private String type;
+    private String material;
+    private double price;
+    private String color;
+    private String brand;
 }
