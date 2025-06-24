cb5e5daf43313a933843ada7779f3834954dc192
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-24 23:52:41 +0530

    working with collections

---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/ApplianceRepository.java
@@ -0,0 +1,7 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.AppliancesDto;
+
+public interface ApplianceRepository {
+    AppliancesDto findAll();
+}
---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/CampingAccessoryRepository.java
@@ -0,0 +1,7 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.CampingAccessoriesDto;
+
+public interface CampingAccessoryRepository {
+    CampingAccessoriesDto findAll();
+}
