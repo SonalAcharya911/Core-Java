19fe3cd57845b497b40a8673c08f549507ff0ae3
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-25 00:06:25 +0530

    working with collections

---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/FurnitureRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.FurnitureDto;
+
+public class FurnitureRepositoryImpl implements FurnitureRepository{
+    @Override
+    public FurnitureDto findaAll() {
+        return null;
+    }
+}
