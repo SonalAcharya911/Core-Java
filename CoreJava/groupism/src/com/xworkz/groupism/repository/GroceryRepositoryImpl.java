3b042532bf7e229f16bdb2aae09a63e8123d5162
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-25 00:06:30 +0530

    working with collections

---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/GroceryRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.GroceryDto;
+
+public class GroceryRepositoryImpl implements GroceryRepository{
+    @Override
+    public GroceryDto findAll() {
+        return null;
+    }
+}
