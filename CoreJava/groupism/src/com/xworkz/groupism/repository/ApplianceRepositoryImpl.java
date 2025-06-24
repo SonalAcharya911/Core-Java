d42d24864c9c96eb56887ff198fbbac390c18287
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-25 00:06:15 +0530

    working with collections

---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/ApplianceRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.AppliancesDto;
+
+public class ApplianceRepositoryImpl implements ApplianceRepository{
+    @Override
+    public AppliancesDto findAll() {
+        return null;
+    }
+}
