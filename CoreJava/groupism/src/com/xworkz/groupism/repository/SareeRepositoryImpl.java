0df086a5a601878223aad16b720488462c7cf46e
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-25 00:06:36 +0530

    working with collections

---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/SareeRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.SareeDto;
+
+public class SareeRepositoryImpl implements SareeRepository{
+    @Override
+    public SareeDto findAll() {
+        return null;
+    }
+}
