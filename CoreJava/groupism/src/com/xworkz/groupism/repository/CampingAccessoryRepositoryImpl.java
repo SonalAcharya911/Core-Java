fd27ec04b0c0af9f8341d73503851a4a4a825601
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-25 00:06:20 +0530

    working with collections

---
+++ CoreJava/groupism/src/com/xworkz/groupism/repository/CampingAccessoryRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.xworkz.groupism.repository;
+
+import com.xworkz.groupism.dto.CampingAccessoriesDto;
+
+public class CampingAccessoryRepositoryImpl implements CampingAccessoryRepository{
+    @Override
+    public CampingAccessoriesDto findAll() {
+        return null;
+    }
+}
