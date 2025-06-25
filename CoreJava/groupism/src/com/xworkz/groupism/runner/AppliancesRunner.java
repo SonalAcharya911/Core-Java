dfe2b13d67c86672f3f6ed3bfd2146ef31999a06
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-26 01:10:00 +0530

    working with collections

--- CoreJava/groupism/src/com/xworkz/groupism/runner/AppliancesRunner.java
+++ CoreJava/groupism/src/com/xworkz/groupism/runner/AppliancesRunner.java
@@ -6,12 +6,15 @@ import com.xworkz.groupism.repository.ApplianceRepositoryImpl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 
 public class AppliancesRunner {
     public static void main(String[] args) {
         ApplianceRepository applianceRepository=new ApplianceRepositoryImpl();
+
         Collection<AppliancesDto> appliancesDtos=applianceRepository.findAll();
+
         System.out.println("appliance info");
         for(AppliancesDto appliancesDto: appliancesDtos){
             System.out.println("Name: "+ appliancesDto.getName());
@@ -20,6 +23,20 @@ public class AppliancesRunner {
             System.out.println("Price: "+ appliancesDto.getPrice());
             System.out.println("Warranty Period: "+ appliancesDto.getWarrantyPeriod());
 
+        }
+
+        System.out.println("using Iterator");
+        Iterator<AppliancesDto> iterator=appliancesDtos.iterator();
+
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("Appliances above 10,000 Rs.");
+        for(AppliancesDto appliancesDto:appliancesDtos){
+            if(appliancesDto.getPrice()>10000){
+                System.out.println("Appliance: "+ appliancesDto.getName()+" costs Rs."+ appliancesDto.getPrice());
+            }
         }
     }
 }
--- CoreJava/groupism/src/com/xworkz/groupism/runner/CampingAccessoriesRunner.java
+++ CoreJava/groupism/src/com/xworkz/groupism/runner/CampingAccessoriesRunner.java
@@ -1,44 +1,41 @@
 package com.xworkz.groupism.runner;
 
 import com.xworkz.groupism.dto.CampingAccessoriesDto;
+import com.xworkz.groupism.repository.CampingAccessoryRepository;
+import com.xworkz.groupism.repository.CampingAccessoryRepositoryImpl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class CampingAccessoriesRunner {
     public static void main(String[] args) {
-        CampingAccessoriesDto campingDto1 = new CampingAccessoriesDto("Sleeping Bag", "Quechua", 1.5, 2499, "Winter");
-        CampingAccessoriesDto campingDto2 = new CampingAccessoriesDto("Camping Tent", "Wildcraft", 3.8, 5499, "All Seasons");
-        CampingAccessoriesDto campingDto3 = new CampingAccessoriesDto("Portable Stove", "Coleman", 2.2, 3199, "All Seasons");
-        CampingAccessoriesDto campingDto4 = new CampingAccessoriesDto("Camping Chair", "Decathlon", 2.0, 1799, "Summer");
-        CampingAccessoriesDto campingDto5 = new CampingAccessoriesDto("Headlamp", "Philips", 0.2, 1299, "Night Use");
-        CampingAccessoriesDto campingDto6 = new CampingAccessoriesDto("Water Bottle", "Hydro Flask", 0.5, 999, "All Seasons");
-        CampingAccessoriesDto campingDto7 = new CampingAccessoriesDto("Backpack", "Trawoc", 1.8, 2199, "All Seasons");
-        CampingAccessoriesDto campingDto8 = new CampingAccessoriesDto("Camping Mat", "Naturehike", 0.9, 1399, "Summer");
-        CampingAccessoriesDto campingDto9 = new CampingAccessoriesDto("First Aid Kit", "Surviveware", 0.6, 899, "All Seasons");
-        CampingAccessoriesDto campingDto10 = new CampingAccessoriesDto("Mosquito Net", "SafariPro", 1.2, 1199, "Monsoon");
 
-        Collection<CampingAccessoriesDto> collection=new ArrayList<>();
+        CampingAccessoryRepository campingAccessoryRepository=new CampingAccessoryRepositoryImpl();
 
-        collection.add(campingDto1);
-        collection.add(campingDto2);
-        collection.add(campingDto3);
-        collection.add(campingDto4);
-        collection.add(campingDto5);
-        collection.add(campingDto6);
-        collection.add(campingDto7);
-        collection.add(campingDto8);
-        collection.add(campingDto9);
-        collection.add(campingDto10);
-
+        Collection<CampingAccessoriesDto> collection=campingAccessoryRepository.findAll();
 
         System.out.println("Camping accessories details");
+
         for(CampingAccessoriesDto campingAccessoriesDto: collection){
             System.out.println("Name: "+ campingAccessoriesDto.getName());
             System.out.println("Brand: "+ campingAccessoriesDto.getBrand());
             System.out.println("Weight: "+ campingAccessoriesDto.getWeight());
             System.out.println("Price: "+ campingAccessoriesDto.getPrice());
             System.out.println("Suitable Weather: "+ campingAccessoriesDto.getSuitableWeather());
+        }
+
+        System.out.println("Accessing using iterator");
+        Iterator<CampingAccessoriesDto> iterator= collection.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("camping accessories suitable for all seasons");
+        for(CampingAccessoriesDto campingAccessoriesDto: collection){
+            if(campingAccessoriesDto.getSuitableWeather().equals("All Seasons")){
+                System.out.println(campingAccessoriesDto);
+            }
         }
 
 
