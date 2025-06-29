64fbdccf03b299d04898557e4a9bd014f862a20f
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-29 22:55:23 +0530

    working on jdbc..

--- CoreJava/birthday-event-form/src/com/xworkz/birthday/repository/BirthdayPartyRepository.java
+++ CoreJava/birthday-event-form/src/com/xworkz/birthday/repository/BirthdayPartyRepository.java
@@ -2,17 +2,19 @@ package com.xworkz.birthday.repository;
 
 import com.xworkz.birthday.dto.BirthdayPartyDto;
 
+import javax.swing.text.html.Option;
+import java.util.Collection;
 import java.util.Optional;
 
 public interface BirthdayPartyRepository {
     boolean persist(BirthdayPartyDto birthdayPartyDto);
 
     default Optional<BirthdayPartyDto[]> findAll(){
-        return java.util.Optional.empty();
+        return Optional.empty();
     }
 
     default Optional<BirthdayPartyDto> findByID(int id) {
-        return java.util.Optional.empty();
+        return Optional.empty();
     }
 
 }
