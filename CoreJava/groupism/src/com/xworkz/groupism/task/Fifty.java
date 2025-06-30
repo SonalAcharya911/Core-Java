a3a6b82d2f7aab1623709dc0dc3d5a7a32c3fc43
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-30 23:24:06 +0530

    Array and Collection

---
+++ CoreJava/groupism/src/com/xworkz/groupism/task/Fifty.java
@@ -0,0 +1,25 @@
+package com.xworkz.groupism.task;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Fifty {
+    public static void main(String[] args) {
+        Collection<Integer> numbers=new ArrayList<>();
+        numbers.add(34);
+        numbers.add(98);
+        numbers.add(21);
+        numbers.add(18);
+        numbers.add(73);
+        numbers.add(45);
+        numbers.add(57);
+
+        Iterator<Integer> iterator=numbers.iterator();
+        while
+
+        while()
+    }
+
+
+}
