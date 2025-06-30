d38788b94dd74f7cd22972aca0f978197aab940d
SonalAcharya911 <sonalacharya911@gmail.com>
2025-06-30 21:54:23 +0530

    Array and Collection

---
+++ CoreJava/groupism/src/com/xworkz/groupism/task/NumbersArray.java
@@ -0,0 +1,22 @@
+package com.xworkz.groupism.task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumbersArray {
+    public static void main(String[] args) {
+        List<Integer> numbers=new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+
+        System.out.println(numbers);
+    }
+}
