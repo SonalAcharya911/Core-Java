2918659bf10be9ecbe3bf53c883e5bfbcd3650f9
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-02 23:32:51 +0530

    Array and Collection

--- CoreJava/groupism/src/com/xworkz/groupism/dto/SareeDto.java
+++ CoreJava/groupism/src/com/xworkz/groupism/dto/SareeDto.java
@@ -3,14 +3,23 @@ package com.xworkz.groupism.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class SareeDto {
+@ToString
+public class SareeDto implements  Comparable<SareeDto> {
     private String occasion;
     private String material;
     private double cost;
     private String color;
     private boolean withBlousePiece;
+
+    @Override
+    public int compareTo(SareeDto o) {
+        System.out.println("running compareTo...");
+       // Double.compare(this.cost,o.cost);
+         return o.material.compareTo(this.material);
+    }
 }
