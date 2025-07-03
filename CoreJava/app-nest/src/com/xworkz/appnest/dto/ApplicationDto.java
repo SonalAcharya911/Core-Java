702e7961981bc4bdc4c31e717b3beefa5403c4d5
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-04 03:11:52 +0530

    collections

--- CoreJava/app-nest/src/com/xworkz/appnest/dto/ApplicationDto.java
+++ CoreJava/app-nest/src/com/xworkz/appnest/dto/ApplicationDto.java
@@ -1,13 +1,23 @@
 package com.xworkz.appnest.dto;
 
 import com.xworkz.appnest.constant.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class ApplicationDto {
     private String appName;
     private Type type;
+    private double size;
     private LocalDate manufactureDate;
-    private OwnerDto owner;
+    private List<OwnerDto> owner;
 
 }
