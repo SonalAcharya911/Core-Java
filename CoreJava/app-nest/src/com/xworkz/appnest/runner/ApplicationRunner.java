3543d222d92c460af60452238a1989560ad56a4c
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-04 03:12:45 +0530

    collections

--- CoreJava/app-nest/src/com/xworkz/appnest/runner/ApplicationRunner.java
+++ CoreJava/app-nest/src/com/xworkz/appnest/runner/ApplicationRunner.java
@@ -1,7 +1,26 @@
 package com.xworkz.appnest.runner;
 
+import com.xworkz.appnest.constant.Type;
+import com.xworkz.appnest.dto.ApplicationDto;
+import com.xworkz.appnest.dto.OwnerDto;
+import com.xworkz.appnest.repository.ApplicationRepository;
+import com.xworkz.appnest.repository.ApplicationRepositoryImpl;
+import com.xworkz.appnest.repository.OwnerRepositoryImpl;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class ApplicationRunner {
     public static void main(String[] args) {
+
+        ApplicationRepository applicationRepository=new ApplicationRepositoryImpl();
+
+        Collection<ApplicationDto> applicationDTOs=applicationRepository.findAll();
+
+        applicationDTOs.stream().forEach(System.out::println);
 
     }
 }
