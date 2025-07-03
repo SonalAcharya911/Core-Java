d4bb32ca525d7acb2450fc456bb50d59379d3040
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-04 03:13:58 +0530

    collections

--- CoreJava/app-nest/src/com/xworkz/appnest/constant/Type.java
+++ CoreJava/app-nest/src/com/xworkz/appnest/constant/Type.java
@@ -1,5 +1,16 @@
 package com.xworkz.appnest.constant;
 
 public enum Type {
-
+    COMMUNICATION,
+    ENTERTAINMENT,
+    PRODUCTIVITY,
+    CREATIVE,
+    EDUCATION,
+    FINANCE,
+    HEALTH,
+    UTILITY,
+    SOCIAL,
+    TRAVEL,
+    SHOPPING,
+    FOOD
 }
