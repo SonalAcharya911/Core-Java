475855b343002836607130d85eaa4797bc423730
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-04 03:12:01 +0530

    collections

--- CoreJava/app-nest/src/com/xworkz/appnest/repository/ApplicationRepository.java
+++ CoreJava/app-nest/src/com/xworkz/appnest/repository/ApplicationRepository.java
@@ -1,4 +1,9 @@
 package com.xworkz.appnest.repository;
 
+import com.xworkz.appnest.dto.ApplicationDto;
+
+import java.util.Collection;
+
 public interface ApplicationRepository {
+    Collection<ApplicationDto> findAll();
 }
