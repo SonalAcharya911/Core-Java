ee8b77b088ce1056410b79e088e8844f310cc371
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-05 23:48:36 +0530

    collections

--- CoreJava/app-nest/src/com/xworkz/appnest/dto/OwnerDto.java
+++ CoreJava/app-nest/src/com/xworkz/appnest/dto/OwnerDto.java
@@ -1,10 +1,8 @@
 package com.xworkz.appnest.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
