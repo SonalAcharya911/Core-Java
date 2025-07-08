dbbd0a533616ad77151de10730ce935e960d7193
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-08 23:55:12 +0530

    collections

---
+++ CoreJava/app-nest/src/com/xworkz/appnest/runner/ApplicationMapRunner.java
@@ -0,0 +1,11 @@
+package com.xworkz.appnest.runner;
+
+import com.xworkz.appnest.dto.ApplicationDto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationMapRunner {
+    Map<ApplicationDto,String> map=new HashMap<>();
+
+}
