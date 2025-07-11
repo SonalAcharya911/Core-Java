a5f0d10bda4b20ee5da6f9d581c746ba482f48e3
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-11 10:38:17 +0530

    dto associations along with Sets

--- CoreJava/linked-customer/src/com/xworkz/linkedCustomer/dto/CompanyDTO.java
+++ CoreJava/linked-customer/src/com/xworkz/linkedCustomer/dto/CompanyDTO.java
@@ -1,2 +1,12 @@
-package com.xworkz.linkedCustomer.dto;public class CompanyDTO {
+package com.xworkz.linkedCustomer.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CompanyDTO {
+    private String name;
+    private String industry;
+    private String location;
+    private CEODTO ceoDto;
 }
