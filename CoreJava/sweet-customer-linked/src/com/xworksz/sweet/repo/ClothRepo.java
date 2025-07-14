512d0819d2c2d743e46283dcd76605c29a9a370c
SonalAcharya911 <sonalacharya911@gmail.com>
2025-07-14 23:56:14 +0530

    set in collection

--- CoreJava/sweet-customer-linked/src/com/xworksz/sweet/repo/ClothRepo.java
+++ CoreJava/sweet-customer-linked/src/com/xworksz/sweet/repo/ClothRepo.java
@@ -8,9 +8,13 @@ import java.util.TreeSet;
 public class ClothRepo {
     public Set<ClothDTO> getClothes(){
         ClothDTO clothDTO1=new ClothDTO("Silk",2,5000);
+        ClothDTO clothDTO2 = new ClothDTO("Chiffon",6,1000);
+        ClothDTO clothDTO3= new ClothDTO("Velvet",4,3200);
 
         Set<ClothDTO> clothDTOSet=new TreeSet<>();
         clothDTOSet.add(clothDTO1);
+        clothDTOSet.add(clothDTO2);
+        clothDTOSet.add(clothDTO3);
 
         return clothDTOSet;
     }
